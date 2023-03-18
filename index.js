2764d18a90d0594737a3fa789b19aed812c01d3f
Brennan Bradley <Brennanb572@gmail.com>
2023-03-18 01:25:22 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if(feet <= 400){
+    return 'This one is on me!';
+  }
+  else if (feet >= 400 && feet <= 2000){
+    return 'That will be twenty bucks.';
+  }
+  else if (feet >= 2000 && feet < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else (feet > 2500)
+    return "No can do.";
+  }
+
+
+function ternaryCheckCity(checkCity){
+  // Write your code here!
+  return checkCity === 'NYC'?'Ok, sounds good.':'No go.'
+  
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(generousTip){
   // Write your code here!
-}
+    switch(generousTip){
+      case 'generous':
+        return "Thank you so much."
+      case 'not as generous':
+        return 'Thank you.'
+      default:
+        return 'Bye.'
+    }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
